cb86e52915c0aab2111f4403ccda320d0770c12e
SpaxR <55739719+SpaxR@users.noreply.github.com>
2023-06-05 21:57:44 +0200

    Removed Template-Components
    Moved Index to Web.Shared

--- CentralStation/Program.cs
+++ CentralStation/Program.cs
@@ -1,12 +1,9 @@
-using CentralStation.Data;
 using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
 builder.Services.AddMudServices();
 
 var app = builder.Build();
