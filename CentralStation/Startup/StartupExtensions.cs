188724ba6abc3803a8c04c492f598bcb6bba92f4
SpaxR <55739719+SpaxR@users.noreply.github.com>
2023-06-18 21:47:50 +0200

    Setup Project-Structure
    Added Directory.Build.props
    Removed default-Values from Projects
    Added StartupExtensions

---
+++ CentralStation/Startup/StartupExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace CentralStation.Startup;
+
+public static class StartupExtensions
+{
+	public static readonly Assembly[] Assemblies =
+		Directory.EnumerateFiles(
+				Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
+				nameof(CentralStation) + "*.dll",
+				SearchOption.TopDirectoryOnly
+			)
+			.Select(Assembly.LoadFrom)
+			.ToArray();
+}
