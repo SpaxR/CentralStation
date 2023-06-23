3939aa6085ec7b825da9e4f73558fdf4d9a39520
SpaxR <55739719+SpaxR@users.noreply.github.com>
2023-06-24 00:37:45 +0200

    Merged shared project back to CentralStation
    Updated Assembly-List to use references

--- CentralStation/Startup/StartupExtensions.cs
+++ CentralStation/Startup/StartupExtensions.cs
@@ -7,14 +7,10 @@ namespace CentralStation.Startup;
 
 public static class StartupExtensions
 {
-	public static readonly Assembly[] Assemblies =
-		Directory.EnumerateFiles(
-				Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
-				nameof(CentralStation) + "*.dll",
-				SearchOption.TopDirectoryOnly
-			)
-			.Select(Assembly.LoadFrom)
-			.ToArray();
+	public static readonly Assembly[] Assemblies = Assembly.GetExecutingAssembly()
+		.GetReferencedAssemblies()
+		.Select(Assembly.Load)
+		.ToArray();
 
 	public static IServiceCollection AddServicesByConvention(this IServiceCollection services, params Assembly[] assemblies)
 	{
