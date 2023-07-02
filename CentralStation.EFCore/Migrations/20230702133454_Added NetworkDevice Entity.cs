5605422816843ba8510e129f043acb95977b2157
SpaxR <55739719+SpaxR@users.noreply.github.com>
2023-07-02 15:35:22 +0200

    Added Migration for Network-Devices

---
+++ CentralStation.EFCore/Migrations/20230702133454_Added NetworkDevice Entity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CentralStation.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNetworkDeviceEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NetworkDevices",
+                schema: "central_station",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HostName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserDefinedName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NetworkDevices", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NetworkDevices",
+                schema: "central_station");
+        }
+    }
+}
