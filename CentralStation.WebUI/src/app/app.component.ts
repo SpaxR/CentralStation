2021d6377737d3c0487da647725e2d6f4730243a
SpaxR <55739719+SpaxR@users.noreply.github.com>
2024-03-23 01:36:37 +0100

    Added Toast-Messages

--- CentralStation.WebUI/src/app/app.component.ts
+++ CentralStation.WebUI/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {ServiceProxiesModule} from "./shared/service-proxies/service-proxies.module";
+import {ToastModule} from "primeng/toast";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ServiceProxiesModule],
+  imports: [RouterOutlet, ServiceProxiesModule, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
