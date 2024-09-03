f39584c62df4932e552695e9a6b6160adc676479
SpaxR <55739719+SpaxR@users.noreply.github.com>
2024-09-03 02:16:55 +0200

    Added pipe to display ip-addresses

---
+++ CentralStation.WebUI/src/app/shared/pipes/ip-address.pipe.spec.ts
@@ -0,0 +1,27 @@
+import { IpAddressPipe } from './ip-address.pipe';
+import { Utils } from '../../modules/networking/utils';
+
+fdescribe('IpAddressPipe', () => {
+  let pipe: IpAddressPipe;
+
+  beforeEach(() => {
+    pipe = new IpAddressPipe();
+  });
+
+  it('create an instance', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  it('should return string of ip-address', () => {
+    const ip = Utils.IpAddressToNumber([127, 0, 0, 1]);
+
+    const result = pipe.transform(ip);
+
+    expect(result).toEqual('127.0.0.1');
+  });
+
+  it('should return zeros without value', () => {
+    const result = pipe.transform();
+    expect(result).toEqual('0.0.0.0');
+  });
+});
