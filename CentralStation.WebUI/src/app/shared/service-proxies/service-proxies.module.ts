72456db03f1d1d1e9b31c953bce9d5e1acff5f29
SpaxR <55739719+SpaxR@users.noreply.github.com>
2024-09-21 01:58:37 +0200

    Updated Angular to Version 18

--- CentralStation.WebUI/src/app/shared/service-proxies/service-proxies.module.ts
+++ CentralStation.WebUI/src/app/shared/service-proxies/service-proxies.module.ts
@@ -1,18 +1,14 @@
 import {NgModule} from '@angular/core';
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import * as Proxy from './service-proxies';
 import {API_BASE_URL} from "./service-proxies";
 import {environment} from "../../../environments/environment";
 
-@NgModule({
-  imports:[
-    HttpClientModule,
-  ],
-  providers: [
-    Proxy.NetworkProxy,
-    Proxy.NetworkDeviceProxy,
-    {provide: API_BASE_URL, useValue: environment.backendUrl}
-  ]
-})
+@NgModule({ imports: [], providers: [
+        Proxy.NetworkProxy,
+        Proxy.NetworkDeviceProxy,
+        { provide: API_BASE_URL, useValue: environment.backendUrl },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class ServiceProxiesModule {
 }
