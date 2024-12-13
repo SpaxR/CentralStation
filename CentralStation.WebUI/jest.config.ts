ed8b55801fa5b491e9729cf003805a8f73a789dc
SpaxR <55739719+SpaxR@users.noreply.github.com>
2024-12-13 23:08:21 +0100

    Configured HappyDom to fix jest-environment

--- CentralStation.WebUI/jest.config.ts
+++ CentralStation.WebUI/jest.config.ts
@@ -2,10 +2,5 @@
 export default {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: [`${__dirname}/tests/setup.ts`],
-  moduleNameMapper: {
-    // '\\.(jpg|jpeg|png)$': `${__dirname}/mock-module.js`,
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-  },
+  testEnvironment: '@happy-dom/jest-environment',
 };
