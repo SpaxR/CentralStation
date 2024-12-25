48d506fd5157c3620a0a0903de67da265e30d08f
SpaxR <55739719+SpaxR@users.noreply.github.com>
2024-12-25 14:10:27 +0100

    Cleaned up test-helpers

--- CentralStation.WebUI/tests/helpers/async.ts
+++
@@ -1,9 +0,0 @@
-﻿import { defer, Observable } from 'rxjs';
-
-export function asyncData<T>(data: T): Observable<T> {
-  return defer(() => Promise.resolve(data));
-}
-
-export function asyncError<T = any>(error: Error|string): Observable<T> {
-  return defer(() => Promise.reject(error));
-}
